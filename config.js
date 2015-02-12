901d98a0f78f6bd69ad27bc6551d4367039b8cff
Roger Stringer <roger@freekrai.net>
2015-02-12 13:20:25 -0800

    added config.js support

---
+++ config.js
@@ -0,0 +1,16 @@
+module.exports = {
+	Site: {
+		Url: "http://mysite.com",
+		Title: 'Sample Site',
+		About: 'My Blog',
+		Author: 'Your Name',
+		DefaultImage: 'http://mysite.com/images/logo.png'
+	},
+	Social: {
+		Twitter: "yourusername",
+		autoTweets: {
+			twitterUserName: "yourusername",
+			twitterClientNeedle: "your_app_needle"
+		}
+	}
+};
