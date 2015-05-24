370f1049014b20cab304d9c01ca9018b450b541b
Anthony <anthonycraig@me.com>
2015-05-24 16:07:11 -0700

    yes

--- camel.js
+++ camel.js
@@ -257,7 +257,8 @@ function generateHtmlAndMetadataForFile(file) {
 			var d = 1;
 			for( var i in tags ){
 				var tag = tags[i].trim();
-				tagslist.push('<a href="/tags/' + tag + '">' + tag.capitalize() + '</a>');
+				tagslist.push('<a href="/tags/' + tag + '">' + tag.toLowerCase() + '</a>');
+//				tagslist.push('<a href="/tags/' + tag + '">' + tag.capitalize() + '</a>');
 				hashtags += tag;
 				if( d < tags.length ){
 					hashtags += ',';
