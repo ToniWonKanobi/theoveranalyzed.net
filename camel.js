9942094419a372d9bf0c0bb8998f3762a4e65c1f
Anthony <anthonycraig@me.com>
2015-06-25 16:17:52 -0700

    boom
    Former-commit-id: 0ffebf1232a6ab88054c957a71354144a51bb4f4 [formerly 68ff65f9b8929d07fe874523f43a1a9d4e07afdc] [formerly 4cc9c834e3e6d562a0d05795787f474f9321f3d3 [formerly 5f757d9b1879638678649f6d702acfe0ff90f22d]]
    Former-commit-id: 1a4e14c70816c9740a4867cb48f0e10b7321c513 [formerly 9c46459c2c5d60c5704754caf31451a902120e65]
    Former-commit-id: 2c70c21a477d7d64ebbb2f6662da61f9cb0b6cf0

--- camel.js
+++ camel.js
@@ -421,61 +421,61 @@ function allPostsSortedAndGrouped(completion) {
 	}
 }
 
-function tweetLatestPost() {
-	if (twitterClient !== null && typeof(config.Social.autoTweets.consumer_key) !== 'undefined') {
-		twitterClient.get('statuses/user_timeline', {screen_name: twitterUsername}, function (error, tweets) {
-			if (error) {
-				console.log(JSON.stringify(error, undefined, 2));
-				return;
-			}
-
-			var lastUrl = null, i = 0;
-			while (lastUrl === null && i < tweets.length) {
-				if (tweets[i].source.has(twitterClientNeedle) &&
-					tweets[i].entities &&
-					tweets[i].entities.urls) {
-					lastUrl = tweets[i].entities.urls[0].expanded_url;
-				} else {
-					i += 1;
-				}
-			}
-
-			allPostsSortedAndGrouped(function (postsByDay) {
-				var latestPost = postsByDay[0].articles[0];
-				var link = latestPost.metadata.SiteRoot + latestPost.metadata.relativeLink;
-
-				if (lastUrl !== link) {
-					console.log('Tweeting new link: ' + link);
-
-					// Figure out how many characters we have to play with.
-					twitterClient.get('help/configuration', function (error, configuration, response) {
-						var suffix = " \n\n";
-						var maxSize = 140 - configuration.short_url_length_https - suffix.length;
-
-						// Shorten the title if need be.
-						var title = latestPost.metadata.Title;
-						if (title.length > maxSize) {
-							title = title.substring(0, maxSize - 3) + '...';
-						}
-
-						var params = {
-							status: title + suffix + link
-						};
-						twitterClient.post('statuses/update', params, function (error, tweet, response) {
-								if (error) {
-									console.log(JSON.stringify(error, undefined, 2));
-								}
-						});
-					});
-				} else {
-					console.log('Twitter is up to date.');
-				}
-			});
-		});
-	} else {
-		console.log('No Tweets');
-	}
-}
+//function tweetLatestPost() {
+//	if (twitterClient !== null && typeof(config.Social.autoTweets.consumer_key) !== 'undefined') {
+//		twitterClient.get('statuses/user_timeline', {screen_name: twitterUsername}, function (error, tweets) {
+//			if (error) {
+//				console.log(JSON.stringify(error, undefined, 2));
+//				return;
+//			}
+//
+//			var lastUrl = null, i = 0;
+//			while (lastUrl === null && i < tweets.length) {
+//				if (tweets[i].source.has(twitterClientNeedle) &&
+//					tweets[i].entities &&
+//					tweets[i].entities.urls) {
+//					lastUrl = tweets[i].entities.urls[0].expanded_url;
+//				} else {
+//					i += 1;
+//				}
+//			}
+//
+//			allPostsSortedAndGrouped(function (postsByDay) {
+//				var latestPost = postsByDay[0].articles[0];
+//				var link = latestPost.metadata.SiteRoot + latestPost.metadata.relativeLink;
+//
+//				if (lastUrl !== link) {
+//					console.log('Tweeting new link: ' + link);
+//
+//					// Figure out how many characters we have to play with.
+//					twitterClient.get('help/configuration', function (error, configuration, response) {
+//						var suffix = " \n\n";
+//						var maxSize = 140 - configuration.short_url_length_https - suffix.length;
+//
+//						// Shorten the title if need be.
+//						var title = latestPost.metadata.Title;
+//						if (title.length > maxSize) {
+//							title = title.substring(0, maxSize - 3) + '...';
+//						}
+//
+//						var params = {
+//							status: title + suffix + link
+//						};
+//						twitterClient.post('statuses/update', params, function (error, tweet, response) {
+//								if (error) {
+//									console.log(JSON.stringify(error, undefined, 2));
+//								}
+//						});
+//					});
+//				} else {
+//					console.log('Twitter is up to date.');
+//				}
+//			});
+//		});
+//	} else {
+//		console.log('No Tweets');
+//	}
+//}
 
 function loadHeaderFooter(file, completion) {
 	fs.exists(templateRoot + file, function(exists) {
