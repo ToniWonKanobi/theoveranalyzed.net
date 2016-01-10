662abbde0403fd77b8a254b467cf3f1f10c4ad21
Anthony Craig <anthonycraig@me.com>
2016-01-09 17:22:55 -0800

    trying https thing 3
    Former-commit-id: 5465d643737995c63e4bf36c46483fe54a80b969 [formerly 43cdcdc460d8d3d2e1101c60bb19ee90c6e681fa] [formerly 9f4de637bd46aee93f3c5f7b42dcb416bf286fa6 [formerly e6e2995751062678120557740f88d1d52cb42f25]]
    Former-commit-id: 81afd66fcc598525d59830de1747393caa299c12 [formerly 3a25e576b40cc636e8613ee951cb3fb80352bc56]
    Former-commit-id: bfc5d0bba29f8878101a0ad69f9c958fca0e3381

--- camel.js
+++ camel.js
@@ -46,13 +46,16 @@ app.use(function (request, response, next) {
 	next();
 });
 
-// Let's see if this works
-var forceSsl = function (req, res, next) {
-   if (req.headers['x-forwarded-proto'] !== 'https') {
-	   return res.redirect(['https://', req.get('Host'), req.url].join(''));
-   }
-   return next();
-};
+// Optionally, force all requests to HTTPS
+if (process.env.FORCE_HTTPS) {
+	app.use(function (request, response, next) {
+		if (request.headers['x-forwarded-proto'] === 'http') {
+			response.redirect(siteMetadata.SiteRoot + request.url);
+		} else {
+			next();
+		}
+	});
+}
 
 var server = http.createServer(app);
 
